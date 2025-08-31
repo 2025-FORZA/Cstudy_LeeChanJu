51262944d8deab7654b298d0940070bd4cd8c162
cjlee429 <chanju3530@sookmyung.ac.kr>
2025-08-31 22:25:48 +0900

    Create SUMMER_Lab05_1.c

---
+++ SUMMER_Lab05_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int n, result = 1;
+    scanf("%d", &n);
+    
+    for (; n>0; n--){
+        result = result*n;
+    }
+    
+    printf("%d", result);
+    
+    return 0;
+}
